294f0d2c32cb74e7412a1e76004f12d222686343
Glitch (glitch-hello-website) <none>
2024-07-21 10:21:55 +0000

    🚛👯 Checkpoint ./index.js:10453528/8170

--- index.js
+++ index.js
@@ -9,8 +9,9 @@ const progressBar = document.getElementById('progressBar');
 const overlay = document.getElementById('overlay');
 const downloadBtn = document.getElementById('downloadBtn');
 let shouldBeDisabled = true;
+let loaded = false;
 
-cosnt expFactor = 8;
+const expansionFactor = 8;
 const encUrl = "https://cdn.glitch.global/c46096bd-2ff8-49e5-984a-c5a008800622/taesd_encoder.onnx?v=1721555115983";
 const decUrl = "https://cdn.glitch.global/c46096bd-2ff8-49e5-984a-c5a008800622/taesd_decoder.onnx?v=1721555116768";
 ort.env.wasm.numThreads = 4;
@@ -29,7 +30,7 @@ imageUpload.addEventListener('change', () => {
     } else {
         shouldBeDisabled = true;
     }
-    generateBtn.disabled = shouldBeDisabled;
+    generateBtn.disabled = shouldBeDisabled & loaded;
 });
 
 
@@ -39,6 +40,8 @@ let gifLoading = Promise.all([fetch('https://cdn.jsdelivr.net/npm/gif.js@0.2.0/d
       throw new Error("Network response was not OK");
     return response.blob();
   }), loadModels()]).then(([workerBlob, [encoderModel, decoderModel]]) => {
+    loaded = true;
+    generateBtn.disabled = shouldBeDisabled;
     console.log("init")
     
 
@@ -55,7 +58,7 @@ let gifLoading = Promise.all([fetch('https://cdn.jsdelivr.net/npm/gif.js@0.2.0/d
         // Show progress bar and overlay
         progressBarContainer.style.display = 'block';
         progressBar.style.width = '0%';
-        progressBar.textContent = '0%';
+        progressBar.textContent = '';
         overlay.style.display = 'flex';
 
         const reader = new FileReader();
@@ -75,7 +78,7 @@ let gifLoading = Promise.all([fetch('https://cdn.jsdelivr.net/npm/gif.js@0.2.0/d
               async function decodeImages(frames) {
                   decoded = []
                   for(let frame of frames) {
-                      const tensor = new ort.Tensor('float32', frame, [1, 4, Math.floor(canvas.height / exp_factor), Math.floor(canvas.width / exp_factor)]); // Adjust shape as needed
+                      const tensor = new ort.Tensor('float32', frame, [1, 4, Math.ceil(canvas.height / expansionFactor), Math.ceil(canvas.width / expansionFactor)]); // Adjust shape as needed
                       const feeds = { sample: tensor };
                       const results = await decoderModel.run(feeds);
                       console.log(results);
@@ -92,11 +95,15 @@ let gifLoading = Promise.all([fetch('https://cdn.jsdelivr.net/npm/gif.js@0.2.0/d
   
               const tensor = new ort.Tensor('float32', normalizedPixels, [1, 3, canvas.height, canvas.width]); // Adjust shape as needed
               const feeds = { sample: tensor };
+              progressBar.textContent = "Encoding image...";
               encoderModel.run(feeds).then(results => {
                   const latentRepresentation = results.latent_sample.data;
 
+
                   const frameCount = parseInt(gifLength.value);
                   const etaValue = parseFloat(eta.value);
+                  progressBar.style.width = '0%';
+                  progressBar.textContent = '0%';
 
                   // Create a new Web Worker
                   const worker = new Worker('worker.js');
@@ -109,52 +116,55 @@ let gifLoading = Promise.all([fetch('https://cdn.jsdelivr.net/npm/gif.js@0.2.0/d
                       } else if (e.data.type === 'result') {
                           const frames = e.data.frames;
                         
-                          frames.map
+                          decodeImages(frames).then(frames => {
 
-                          const gif = new GIF({
-                              workers: 2,
-                              quality: 10,
-                              width: canvas.width,
-                              height: canvas.height,
-                              workerScript: URL.createObjectURL(workerBlob),
-                          });
+                            const gif = new GIF({
+                                workers: 2,
+                                quality: 10,
+                                width: canvas.width,
+                                height: canvas.height,
+                                workerScript: URL.createObjectURL(workerBlob),
+                            });
 
-                          frames.forEach(frame => {
-                              // Convert back to [0, 255] range for display
-                              const displayPixels = new Uint8ClampedArray(frame.length);
-                              for (let i = 0; i < frame.length; i++) {
-                                  displayPixels[i] = Math.round((frame[i] + 1) * 127.5);
-                              }
-                              const frameData = new ImageData(displayPixels, canvas.width, canvas.height);
+                            frames.forEach(frame => {
+                                // Convert back to [0, 255] range for display
+                                const displayPixels = new Uint8ClampedArray(frame.length);
+                                for (let i = 0; i < frame.length; i++) {
+                                    displayPixels[i] = Math.round((frame[i] + 1) * 127.5);
+                                }
+                                const frameData = new ImageData(displayPixels, canvas.width, canvas.height);
 
-                              const frameCanvas = document.createElement('canvas');
-                              frameCanvas.width = canvas.width;
-                              frameCanvas.height = canvas.height;
-                              frameCanvas.getContext('2d').putImageData(frameData, 0, 0);
+                                const frameCanvas = document.createElement('canvas');
+                                frameCanvas.width = canvas.width;
+                                frameCanvas.height = canvas.height;
+                                frameCanvas.getContext('2d').putImageData(frameData, 0, 0);
 
-                              gif.addFrame(frameCanvas, {delay: 100});
-                          });
+                                gif.addFrame(frameCanvas, {delay: 100});
+                            });
 
-                          gif.on('finished', function(blob) {
-                              const gifUrl = URL.createObjectURL(blob);
-                              result.innerHTML = `<img src="${gifUrl}" alt="Generated GIF">`;
-                              downloadBtn.href = gifUrl;
-                              downloadBtn.style.display = "inline";
-                              const fileNameWithoutExtension = file.name.substring(0, file.name.lastIndexOf('.'));
-                              downloadBtn.setAttribute('download', `diffused-${fileNameWithoutExtension}-${frameCount}-steps-eta${etaValue}.gif`);
-                              // Hide progress bar and overlay
-                              progressBarContainer.style.display = 'none';
-                              overlay.style.display = 'none';
-                              generateBtn.disabled = shouldBeDisabled;
-                          });
+                            gif.on('finished', function(blob) {
+                                const gifUrl = URL.createObjectURL(blob);
+                                result.innerHTML = `<img src="${gifUrl}" alt="Generated GIF">`;
+                                downloadBtn.href = gifUrl;
+                                downloadBtn.style.display = "inline";
+                                const fileNameWithoutExtension = file.name.substring(0, file.name.lastIndexOf('.'));
+                                downloadBtn.setAttribute('download', `diffused-${fileNameWithoutExtension}-${frameCount}-steps-eta${etaValue}.gif`);
+                                // Hide progress bar and overlay
+                                progressBarContainer.style.display = 'none';
+                                overlay.style.display = 'none';
+                                generateBtn.disabled = shouldBeDisabled;
+                            });
 
-                          gif.render();
+                            gif.render();
+                            
+                          })
                       }
                   };
 
+                  console.log(latentRepresentation, latentRepresentation.length);
                   // Send data to the worker
                   worker.postMessage({
-                      latentRepresentation,
+                      normalizedPixels: latentRepresentation,
                       frameCount,
                       etaValue
                   });
